dbe48a565eba06bf47b8687017b17ca1e52988a9
Amit Gupta <amit@gprof.com>
2021-10-08 08:30:37 -0700

    Initial Commit

---
+++ secondOpinion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  secondOpinion
+//
+//  Created by Amit Gupta on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
